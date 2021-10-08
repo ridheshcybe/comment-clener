c26cc83be1ca514c4d729258f55223fd5867c35a
cybemachine <80441770+cybemachine@users.noreply.github.com>
2021-10-08 23:45:54 +0545

    added whole lot

--- modules.js
+++ modules.js
@@ -1,31 +1,36 @@
+function regexall(e) {
+    return new Promise((l, r) => {
+        l(e.replaceAll(/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g, ""))
+    })
+}
 module.exports = {
-    js: (data) => {
-        return new Promise((res, rej) => {
-            res(data.replaceAll(/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g, ''));
-        })
-    },
-    css: (data) => {
-        return new Promise((res, r) => {
-            res(data.replaceAll(/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g, ''););
-        })
-    },
-    html: (data) => {
-            res(data.replaceAll(/<!--(.|\s)*?-->/g, ''));
-        })
-    },
-    htm: (data) => html(data),
-    basic: (data) => {
-        return new Promise((res, rej) => {
-            var processi = 
-            res(data.replaceAll(/REM.*[\r\n]/g, ''));
-        })
-    },
-    py: (data) => {
-        return new Promise((res, rej) => {
-            res(data.replaceAll(/#.*[\r\n]/g, ''))
-        })
-    },
-    java: (data) => {
-
-    };
+    html: e => new Promise((l, r) => {
+        l(e.replaceAll(/<!--(.|\s)*?-->/g, ""))
+    }),
+    basic: e => new Promise((l, r) => {
+        l(e.replaceAll(/REM.*[\r\n]/g, ""))
+    }),
+    py: e => new Promise((l, r) => {
+        l(e.replaceAll(/#.*[\r\n]/g, ""))
+    }),
+    asm: e => new Promise((l, r) => {
+        l(e.replaceAll(/;.*[\r\n]/g, ""))
+    }),
+    sql: e => new Promise((l, r) => {
+        l(e.replaceAll(/(--.*[\r\n])|((\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*))/g, ""))
+    }),
+    go: e => regexall(e),
+    groovy: e => regexall(e),
+    gvy: e => regexall(e),
+    gy: e => regexall(e),
+    gsh: e => regexall(e),
+    java: e => regexall(e),
+    css: e => regexall(e),
+    php: e => regexall(e),
+    ts: e => regexall(e),
+    cpp: e => regexall(e),
+    js: e => regexall(e),
+    cs: e => regexall(e),
+    c: e => regexall(e),
+    htm: e => html(e)
 };
