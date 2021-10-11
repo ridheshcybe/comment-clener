dd07942e93cf5db196a80f0e42e94d15fd0c223e
cybemachine <80441770+cybemachine@users.noreply.github.com>
2021-10-11 20:29:05 +0545

    strict

--- src/window.js
+++ src/window.js
@@ -1,21 +1,22 @@
 if (typeof window === 'undefined') {
     if (module.parent) return module.exports = require('./node');
-    require('./node')(require('process').argv[2], reqire('path').extname(require('process').argv[2]))
-} else {
-    import Cleaner from './modules.js';
-    window.Commentclener = (code, lang, log = false) => new Promise((l, r) => {
-        var cleaner = new Cleaner(code, lang);
-        cleaner.run().then((e) => {
-            var {
-                txt
-            } = e;
+    require('./node.js')(require('process').argv[2], reqire('path').extname(require('process').argv[2]));
+    return;
+};
 
-            if (log) {
-                console.log(txt)
-            };
+import Cleaner from './modules.js';
 
-            l(e);
-        })
-    });
+window.Commentclener = (code, lang, log = false) => new Promise((l, r) => {
+    var cleaner = new Cleaner(code, lang);
+    cleaner.run().then((e) => {
+        var {
+            txt
+        } = e;
 
-}
+        if (log) {
+            console.log(txt);
+        };
+
+        l(e);
+    })
+});
